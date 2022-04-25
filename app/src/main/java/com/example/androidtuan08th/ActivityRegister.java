674c37f142ca53b5b26ade6ad1dd1532b5fecdb7
TrungNgocCode <trungngoccode@gmail.com>
2022-04-25 17:20:51 +0700

    finish register firebase

--- app/src/main/java/com/example/androidtuan08th/ActivityRegister.java
+++ app/src/main/java/com/example/androidtuan08th/ActivityRegister.java
@@ -1,14 +1,72 @@
 package com.example.androidtuan08th;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ActivityRegister extends AppCompatActivity {
+    private Button btnSignIn;
+    private EditText edtName;
+    private EditText edtEmail;
+    private EditText edtPass;
+    private EditText edtPassRe;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        edtName = findViewById(R.id.edtName);
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPass = findViewById(R.id.edtPass);
+        edtPassRe = findViewById(R.id.edtPassRe);
+        btnSignIn = findViewById(R.id.btnSignIn);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String pass = edtPass.getText().toString();
+                if (!pass.equalsIgnoreCase(edtPassRe.getText().toString())){
+                    Toast.makeText(ActivityRegister.this, "Type password again", Toast.LENGTH_SHORT).show();
+                }else{
+                    mAuth = FirebaseAuth.getInstance();
+                    String email = edtEmail.getText().toString();
+
+                    mAuth.createUserWithEmailAndPassword(email, pass)
+                            .addOnCompleteListener(ActivityRegister.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        Toast.makeText(ActivityRegister.this, "Create account successfull: " + user.getEmail(),
+                                                Toast.LENGTH_SHORT).show();
+                                    } else {
+                                        Log.d("tag",task.toString());
+                                        Toast.makeText(ActivityRegister.this, "Create account fail: " + task.toString(),
+                                                Toast.LENGTH_SHORT).show();
+
+                                    }
+                                }
+                            });
+
+
+                }
+            }
+        });
     }
 }
